db319ad9395457c863eef89d7578b524451a74dc
Fabian9124 <fabi.morales@duocuc.cl>
2026-04-24 00:50:23 -0400

    Se agrega el atributo apellido, se quita password en editar ususario y Se actualizan funciones Crud y se  modifica funcion editar usuario para que cambie por atributo independiente, se crea una funcion llamada cambio de passwod

--- app/schemas/usuario.py
+++ app/schemas/usuario.py
@@ -1,26 +1,35 @@
 from pydantic import BaseModel
 
+
+# Clase create usuario para crear usuario 
 class UsuarioCreate(BaseModel):
     nombre: str
+    apellido: str
     email: str
     password: str
     rol: str = "USER"
     estado: bool = True
 
-
+# Clase update usuario para cambios parciales
 class UsuarioUpdate(BaseModel):
     nombre: str | None = None
+    apellido: str | None = None
     email: str | None = None
-    password: str | None = None
     rol: str | None = None
     estado: bool | None = None
 
+class UsuarioPasswordUpdate(BaseModel):
+    password: str
+
+# Clase respuesta para mostrar usuario 
 class UsuarioResponse(BaseModel):
     id: int
     nombre: str
+    apellido: str
     email: str
     rol: str
     estado: bool
 
+    # Para que Pydantic trabaje con SQLAlchemy
     class Config:
         from_attributes = True
